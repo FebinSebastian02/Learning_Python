955d2ea90dd27578178daa722ad965763c8b8800
Febin Sebastian <febin.vadakkan@gmail.com>
2023-07-10 13:46:59 +0530

    Commiting OOPs introduction file

---
+++ IntroductionToOop.py
@@ -0,0 +1,41 @@
+#Python is famous as it supports:- a) Functional Programming
+#                                  b) Object Oriented Programming
+#                                  c) Prodedure Oriented Programming
+
+# Object Oriented Programming has mainly 2 concepts:-
+#     1) Objects
+#     2) Classes
+#
+# 1) Objects
+# In programming world, we try to solve real world problem with virtual world solution. In real world we use objects for
+# our needs and they are important. eg:- A camera is used for taking a photo, a laptop is used to watch movie etc.
+# Even humans are treated as objects. eg:- Suppose I have a company and I have a task that needs to be done.
+# For completing this task, I need employees. So for me, employees are objects that are assigned tasks to perform.
+# These employees also use other objects like AC, fan, laptop etc while working.
+#
+# Object have 2 things:- a) Attributes b) Behaviours
+#
+# a) Attributes
+# They are properties / data of an object.
+# eg:- I as a person is an object. I have a name, age, height etc or I know something. These are my attributes.
+# An object have something where we can store data. To store data, we use variables. These are called the attributes
+#
+# b) Behaviours
+# These are the actions done by an object.
+# eg:- I as a person is an object. I do reading, walking, sleeping etc or I do something based on what I know. These are my
+# behaviours.
+# An object will have some behaviour also. To define a behaviour, we need methods. Methods are the functions used in OOP.
+
+# 2) Classes
+# All objects in real world like camera, laptop etc are manufactured somewhere.
+# eg:- Realme phone I use is not the only phone manufactured. There are millions of same phone manufactured.
+# The important thing is where these phones were designed - In realme factory.
+# That is, this phone was designed once and manufactured multiple times with the same design.
+#
+# This design is called Class in OOP and the phones are called Objects in OOP.
+#
+# Class is also called Blueprint
+# Object is called Real stuff / Instance.
+# So, object is an instance of a class. The design or class when brought into reality is called object.
+#
+# Therefore, if we have class, we can make many objects.
