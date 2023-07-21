b4ee409ba4cd9dd03e4ba114acb30bafa39fd8cb
Febin Sebastian <febin.vadakkan@gmail.com>
2023-07-21 10:29:05 +0530

    Commiting Intro to Polymorphism file

---
+++ IntroductionToPolymorphism.py
@@ -0,0 +1,10 @@
+#Polymorphism -> Poly = many, morph = form.
+#That is, 1 thing can take multiple forms
+#eg:- As humans, we are polymorphic because we behave in different forms according to different situation
+#In the same way, in OOPs, the objects have different forms
+
+#There are 4 ways to implement Polymorphism:-
+#a) Duck typing
+#b) Operator overloading
+#c) Method overloading
+#d) Method overriding
